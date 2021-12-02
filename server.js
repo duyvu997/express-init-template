a20ee96156184961fe02f11c441ef2f0856020de
duyvu997 <ngocduy0997@gmail.com>
2021-12-03 00:33:48 +0700

    remove log

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const fs = require('fs');
-const path = require('path');
-const morgan = require('morgan');
 const cors = require('cors');
 const errorHandler = require('./middlewares/error-handler');
 const routes = require('./routes')
 require('dotenv').config();
 
-
 const PORT = process.env.PORT || 3000;
-const logStream = fs.createWriteStream(path.join(__dirname, 'app.log'), {
-  flags: 'a',
-});
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
-app.use(morgan('common', { stream: logStream }));
 app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 
 app.use(routes());
